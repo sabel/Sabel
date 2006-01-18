ac76870fb22ce4d30206b076e774c82c6a6e353b
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-01-18 11:34:33 +0000

    move this file.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@22 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sample/app/modules/Defaults/views/templates/Default/top.tpl.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Sample Blogs.</title>
+  </head>
+<body>
+
+<? linkTo(array(LNAME=>'¥Ö¥í¥°',
+                MODULE=>'Show', CONTROLLER=>'blog', METHOD=>'show',
+                ABSOLUTE=>true)) ?>
+
+<hr/>
+HOME | privacy policy | µ¬Ìó | ±¿±Ä²ñ¼Ò 
+<hr/>
+
+</body>
+</html>
