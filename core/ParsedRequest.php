797c6a43418b83d15d4d9f49bdf6fa3856b08237
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-02-15 09:35:40 +0000

    move file.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@25 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ core/ParsedRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+class ParsedRequest
+{
+  private $request;
+
+  public function __construct($request)
+  {
+    $this->request = $request;
+  }
+
+  public function getModule()
+  {
+    if (!empty($this->request[0]) && $this->request[0] != "") {
+      return $this->request[0];
+    } else {
+      return 'Defaults';
+    }
+  }
+
+  public function getController()
+  {
+    if (isset($this->request[1]) && $this->request[1] != "") {
+      return $this->request[1];
+    } else {
+      return 'Default';
+    }
+  }
+
+  public function getAction()
+  {
+    if (isset($this->request[2]) && $this->request[2] != "") {
+      return $this->request[2];
+    } else {
+      return 'top';
+    }
+  }
+
+  public function getParameter()
+  {
+    return $this->request[3];
+  }
+}
+
+?>
