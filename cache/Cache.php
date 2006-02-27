c31d466c14d8016fa085c70c40ed08178826ef8c
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-02-27 10:05:18 +0000

    change weigth value.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@43 0a2fe512-f36d-da11-803c-00304871b6ac

--- cache/Cache.php
+++ cache/Cache.php
@@ -15,8 +15,8 @@ class MemCacheImpl extends Cache
   public function __construct()
   {
     $this->memcache = new Memcache();
-    $this->memcache->addServer('192.168.0.197', 11211, true, 1);
-    $this->memcache->addServer('192.168.0.191', 11211, true, 3);
+    $this->memcache->addServer('192.168.0.197', 11211, true);
+    $this->memcache->addServer('192.168.0.191', 11211, true);
 
     $this->memcache->addServer('127.0.0.1', 11211, true, 1);
   }
