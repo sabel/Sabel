13400ab041a2e391d43472e0d06d7094fc226f1c
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-04-28 06:21:47 +0000

    smarty mb_substr method make
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@100 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ third/Smarty/plugins/modifier.mb_substr.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty mb_substr modifier plugin
+ *
+ * @param string
+ * @param integer
+ * @param string
+ * @param boolean
+ * @param boolean
+ * @return string
+ */
+function smarty_modifier_mb_substr($string, $length = 80, $postfix = '...')
+{
+  if ($length == 0)
+    return '';
+
+  if (strlen($string) > $length) {
+				return mb_substr($string, 0, $length, 'EUC-JP').$postfix;
+  } else {
+    return $string;
+  }
+}
+
+?>
