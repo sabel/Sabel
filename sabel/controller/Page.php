2f56dd3f29d788ea04b0908f0887e74b5f1fc4ac
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-07-06 07:53:45 +0000

    move and add
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@181 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/controller/Page.php
@@ -0,0 +1,231 @@
+<?php
+
+require_once('Parameters.php');
+require_once('Response.php');
+require_once('DirectoryList.php');
+
+/**
+ * page controller base class.
+ *
+ * @author Mori Reo <mori.reo@servise.jp>
+ * @package sabel.controller
+ */
+abstract class SabelPageController
+{
+  protected $request, $response, $template, $cache, $logger;
+
+  /**
+   * implement for inherit class.
+   *
+   */
+  abstract function initialize();
+  abstract function index();
+
+  /**
+   * get request parameter
+   *
+   * @param string input name
+   * @return mixed
+   */
+  public function __get($name)
+  {
+    $safe = substr($name, 0, 4);
+    
+    if ($safe == 'safe') {
+      $lower = strtolower(substr($name, 4, (strlen($name) - 4)));
+      return Sanitize::sqlSafe($this->request->$lower);
+    }
+    
+    if (! is_null($request = $this->request->$name)) {
+      return $request;
+    } else {
+      return $this->parameters->$name;
+    }
+  }
+  
+  public function setup($request)
+  {
+    $this->container = new SabelDIContainer();
+    
+    $this->request = $request;
+    $this->setupLogger();
+    $this->setupResponse();
+    $this->setTemplate(new HtmlTemplate());
+    $this->setupConfig();
+    $this->setupCache();
+  }
+  
+  protected function setupLogger()
+  {
+    $this->logger = $this->container->load('Core_Logger_File');
+  }
+  
+  protected function setupResponse()
+  {
+    $this->response = new WebResponse();
+  }
+  
+  protected function setupConfig()
+  {
+    $this->config = CachedConfigImpl::create();
+  }
+  
+  protected function setupCache()
+  {
+    $conf = $this->config->get('Memcache');
+    $this->cache = MemCacheImpl::create($conf['server']);
+  }
+  
+  public function execute($method)
+  {
+    $this->checkValidateMethodAndExecute($method);
+    $this->methodExecute($method);
+    $this->initTemplate();
+    $this->showTemplate();
+  }
+  
+  protected function methodExecute($methodName)
+  {
+    $r = ParsedRequest::create();
+    $controllerClass = $r->getModule() . '_' . $r->getController();
+    $refMethod = new ReflectionMethod($controllerClass, $methodName);
+    
+    $hasClass = false;
+    foreach ($refMethod->getParameters() as $paramidx => $parameter) {
+      $requireParameterClass = 
+                  ($reflectionClass = $parameter->getClass()) ? true : false;
+                  
+      if ($requireParameterClass) {
+        $hasClass = true;
+        $this->container = new SabelDIContainer();
+        $object = $this->container->load($reflectionClass->getName());
+      }
+    }
+    
+    if ($hasClass) {
+      $this->$methodName($object);
+    } else {
+      $this->$methodName();
+    }
+    
+  }
+  
+  protected function assignTemplates()
+  {
+    foreach ($this->response->responses() as $key => $val)
+      $this->template->assign($key, $val);
+  }
+
+  protected function checkValidateMethodAndExecute($method)
+  {
+    if ($this->hasValidateMethod($method)) {
+      return $this->executeValidateMethod($method);
+    } else {
+      return true;
+    }
+  }
+
+  protected function executeValidateMethod($method)
+  {
+    $validateMethod = 'validate' . ucfirst($method);
+    return $this->$validateMethod();
+  }
+
+  public function hasMethod($name)
+  {
+    if (method_exists($this, $name)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  protected function setTemplate($template)
+  {
+    $this->template = $template;
+  }
+
+  protected function getActionMethods()
+  {
+    $methods = get_class_methods($this);
+
+    $ar = array();
+    foreach ($methods as $key => $val) {
+      if ($val[0] != '_') {
+        $ar[$key] = $val;
+      }
+    }
+    return $ar;
+  }
+
+  protected function hasValidateMethod($methodName)
+  {
+    $methods = $this->getActionMethods();
+    $vMethodName =(string) 'validate'. ucfirst($methodName);
+
+    $found = false;
+    foreach ($methods as $k => $method) {
+      if ($method === $vMethodName) $found = true;
+    }
+
+    return $found;
+  }
+
+  protected function checkReferer($validURIs)
+  {
+    $ref = $_SERVER['HTTP_REFERER'];
+    $replaced = preg_replace('/\\//', '\/', $validURIs[0]);
+    $patternAbsoluteURI = '/http:\/\/' . $_SERVER['HTTP_HOST'].$replaced.'/';
+    preg_match($patternAbsoluteURI, $ref, $matchs);
+    return (isset($matchs[0])) ? true : false;
+  }
+
+  /**
+   * HTTP Redirect to another location.
+   * this method will be avoid "back button" problem.
+   *
+   * @param string $to /Module/Controller/Method
+   */
+  protected function redirect($to)
+  {
+    $absolute  = 'http://' . $_SERVER['HTTP_HOST'];
+    $redirect  = 'Location: ' . $absolute . $to;
+    header($redirect);
+
+    exit; // exit after HTTP Header(30x)
+  }
+
+  /**
+   * forwaring anothor controller or method of same controller.
+   *
+   */
+  protected function forward($to)
+  {
+    // @todo implemen
+  }
+
+  /**
+   * initialize template
+   */
+  protected function initTemplate()
+  {
+    $d = TemplateDirectorFactory::create();
+    $this->template->selectPath($d->decidePath());
+    $this->template->selectName($d->decideName());
+  }
+
+  /**
+   * process template then rendering it.
+   *
+   */
+  protected function showTemplate()
+  {
+    try {
+      $this->template->rendering();
+    } catch(SabelException $e) {
+      $e->printStackTrace();
+    }
+  }
+}
+
+?>
---
+++ sabel/core/Context.php
@@ -0,0 +1,62 @@
+<?php
+
+class SabelContext
+{
+  private static $parameters = array();
+  
+  private static $includePath = array();
+
+  public static function getController()
+  {
+    // @TODO configuration.
+    return new SabelPageWebController();
+  }
+
+  public static function setParameter($name, $value)
+  {
+    self::$parameters[$name] = $value;
+  }
+
+  public static function getParameter($name)
+  {
+    return self::$parameters[$name];
+  }
+
+  public static function getRequestModule()
+  {
+    return $_REQUEST['module'];
+  }
+
+  public static function getRequestAction()
+  {
+    return $_REQUEST['action'];
+  }
+
+  public static function getLogger()
+  {
+    return FileLogger::singleton();
+  }
+  
+  public static function getContainer()
+  {
+    return new SabelDIContainer();
+  }
+  
+  public static function getCache()
+  {
+    $config = CachedConfigImpl::create()->get('Memcache');
+    return MemCacheImpl::create($config['server']);
+  }
+  
+  public static function addIncludePath($path)
+  {
+    self::$includePath[] = $path;
+  }
+  
+  public static function getIncludePath()
+  {
+    return self::$includePath;
+  }
+}
+
+?>
