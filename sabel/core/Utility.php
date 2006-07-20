46c86534bca61d7c54dab648cccd85911b43eaed
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-07-20 03:58:01 +0000

    refactoring.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@262 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/core/Utility.php
+++ sabel/core/Utility.php
@@ -5,20 +5,20 @@ class Sanitize
   /**
    * remove non alphabet and non numeric characers.
    *
-   * @param mixed  $targe string or array
+   * @param mixed  $target string or array
    * @param string $expected
    * @return mixed cleaned string or array
    */
-  public static function removeNonAlphaNumeric($target, $expected = null)
+  public static function removeNonAlphaNumeric(&$target, $expected = '')
   {
-    $expected = ($expected) ? $expected : "";
+    $cleaned = null;
 
     if(is_array($target)) {
-      foreach ($target as $key => $clean) {
-        $cleaned[$key] = preg_replace( "/[^${$expected}a-zA-Z0-9]/", '', $clean);
+      foreach ($target as $key => $value) {
+        $cleaned[$key] = preg_replace( "/[^${$expected}a-zA-Z0-9]/", '', $value);
       }
     } else {
-      $cleaned = preg_replace( "/[^${$expected}a-zA-Z0-9]/", "", $target);
+      $cleaned = preg_replace( "/[^${$expected}a-zA-Z0-9]/", '', $target);
     }
 
     return $cleaned;
@@ -33,19 +33,21 @@ class Sanitize
     return addslashes($target);
   }
 
-  public static function normalize($target)
+  public static function normalize(&$target)
   {
+    $cleaned = null;
+
     if (get_magic_quotes_gpc()) {
       if (is_array($target)) {
-        foreach ($target as $k => $v) {
-          $target[$k] = stripslashes($v);
+        foreach ($target as $key => $value) {
+          $cleaned[$k] = stripslashes($value);
         }
       } else {
-        $target = stripslashes($target);
+        $cleaned = stripslashes($target);
       }
     }
 
-    return $target;
+    return $cleaned;
   }
 }
 
