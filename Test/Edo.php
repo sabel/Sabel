eff7c87c4a9e850fac0431a1517b0a22267f39ca
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-08-08 07:25:09 +0000

    r2344@Reos-MacBookPro:  morireo | 2006-08-08 16:24:26 +0900  refactoring
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@313 0a2fe512-f36d-da11-803c-00304871b6ac

--- Test/Edo.php
+++ Test/Edo.php
@@ -15,7 +15,6 @@ require_once('sabel/Functions.php');
 require_once('sabel/core/Context.php');
 
 require_once "sabel/edo/RecordObject.php";
-require_once "sabel/edo/RecordClasses.php";
 require_once "sabel/edo/DBConnection.php";
 
 require_once "sabel/edo/Query.php";
@@ -892,6 +891,123 @@ class PgsqlHelper
   }
 }
 
+abstract class BaseUserRecordObject extends Sabel_Edo_RecordObject
+{
+  protected $myChildren         = null;
+  protected $myChildConstraints = array();
+
+  public function __construct($param1 = null, $param2 = null)
+  {
+    $this->setEDO('user', 'pdo');
+    parent::__construct($param1, $param2);
+  }
+
+  public function getMyChildren()
+  {
+    return $this->myChildren;
+  }
+
+  public function getMyChildConstraint()
+  {
+    return $this->myChildConstraints;
+  }
+}
+
+abstract class BaseTreeRecord extends Sabel_Edo_RecordObject
+{
+  protected $structure = 'tree';
+
+  public function __construct($param1 = null, $param2 = null)
+  {
+    $this->setEDO('user', 'pdo');
+    parent::__construct($param1, $param2);
+  }
+
+  public function getStructure()
+  {
+    return $this->structure;
+  }
+
+  protected function getRoot()
+  {
+    return $this->select("{$this->table}_id", 'null');
+  }
+}
+
+abstract class BaseMailRecordObject extends Sabel_Edo_RecordObject
+{
+
+}
+
+// for unit-test
+class Test extends BaseUserRecordObject
+{
+  protected $selectType = Sabel_Edo_RecordObject::WITH_PARENT_OBJECT;
+
+  public function getCondition()
+  {
+    return $this->conditions;
+  }
+
+  public function unsetCondition()
+  {
+    $this->conditions = array();
+  }
+
+  public function getData()
+  {
+    return $this->data;
+  }
+}
+
+class Customer extends BaseUserRecordObject
+{
+  protected $myChildren = array('customer_order','customer_telephone');
+  protected $defaultChildConstraints = array('limit' => 10); // (for telephone)
+
+  public function __construct($param1 = null, $param2 = null)
+  {
+    $this->myChildConstraints['customer_order'] = array('limit' => 10);
+    parent::__construct($param1, $param2);
+  }
+}
+
+class Customer_Order extends BaseUserRecordObject
+{
+  protected $myChildren = 'order_line';
+
+  public function __construct($param1 = null, $param2 = null)
+  {
+    $this->myChildConstraints['order_line'] = array('limit' => 10);
+    parent::__construct($param1, $param2);
+  }
+}
+
+class Child_Record extends BaseUserRecordObject
+{
+  public function __construct($table = null)
+  {
+    parent::__construct();
+
+    if (!is_null($table)) $this->table = $table;
+  }
+}
+
+class Tree extends BaseTreeRecord
+{
+
+}
+
+class Common_Record extends Sabel_Edo_RecordObject
+{
+  public function __construct($table = null)
+  {
+    $this->setEDO('user', 'pdo');
+    parent::__construct();
+
+    if (!is_null($table)) $this->table = $table;
+  }
+}
 
 if (PHPUnit2_MAIN_METHOD == "Test_Edo::main") {
     Test_Edo::main();
