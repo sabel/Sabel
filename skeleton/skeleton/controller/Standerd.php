e6c111d4f8c4ec1a935f9e05fde2c6adaecbee21
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-08-21 11:43:44 +0000

    minor fix for update of Sabel_Request_Request
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@402 0a2fe512-f36d-da11-803c-00304871b6ac

--- skeleton/skeleton/controller/Standerd.php
+++ skeleton/skeleton/controller/Standerd.php
@@ -26,7 +26,7 @@ class Index_<? echo ucfirst($controllerName) ?> extends Sabel_Controller_Page
   public function create()
   {
     if ($this->request->isPost()) {
-      $model = new <? echo $controllerName ?>($this->request->getByName('id'));
+      $model = new <? echo $controllerName ?>($this->request->getUri()->getByName('id'));
       foreach ($this->request->requests() as $name => $value) {
         $model->$name = $value;
       }
@@ -37,7 +37,7 @@ class Index_<? echo ucfirst($controllerName) ?> extends Sabel_Controller_Page
   
   public function edit()
   {
-    $model = new <? echo ucfirst($controllerName) ?>($this->request->getByName('id'));
+    $model = new <? echo ucfirst($controllerName) ?>($this->request->getUri()->getByName('id'));
     
     if ($this->request->isPost()) {
       foreach ($this->request->requests() as $name => $value) {
@@ -52,7 +52,7 @@ class Index_<? echo ucfirst($controllerName) ?> extends Sabel_Controller_Page
   
   public function delete()
   {
-    $model = new <? echo ucfirst($controllerName) ?>($this->request->getByName('id'));
+    $model = new <? echo ucfirst($controllerName) ?>($this->request->getUri()->getByName('id'));
     $model->remove();
     $this->redirect('/index/<? echo $controllerName ?>/lists');
   }
