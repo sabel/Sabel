2fb1534cddda0978f18e182f4d04bf2b1a314c2b
svnebine <svnebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-09-05 05:07:49 +0000

    sorry.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@549 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/db/schema/Setter.php
@@ -0,0 +1,30 @@
+<?php
+
+class Sabel_DB_Schema_TypeSetter
+{
+  public static function set($co, $type)
+  {
+    if ($type === 'boolean' || $type === 'bool') {
+      $co->type = Sabel_DB_Schema_Type::BOOL;
+    } else if ($type === 'date') {
+      $co->type = Sabel_DB_Schema_Type::DATE;
+    } else if ($type === 'time') {
+      $co->type = Sabel_DB_Schema_Type::TIME;
+    } else {
+      $tInt   = new Sabel_DB_Schema_TypeInt();
+      $tStr   = new Sabel_DB_Schema_TypeStr();
+      $tText  = new Sabel_DB_Schema_TypeText();
+      $tTime  = new Sabel_DB_Schema_TypeTime();
+      $tByte  = new Sabel_DB_Schema_TypeByte();
+      $tOther = new Sabel_DB_Schema_TypeOther();
+
+      $tInt->add($tStr);
+      $tStr->add($tText);
+      $tText->add($tTime);
+      $tTime->add($tByte);
+      $tByte->add($tOther);
+
+      $tInt->send($co, $type);
+    }
+  }
+}
