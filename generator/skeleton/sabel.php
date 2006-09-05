5532e023f5b078cb45c0303b152ef4c9dbd5f4c3
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-09-05 05:31:20 +0000

    r2818@Reos-MacBookPro:  morireo | 2006-09-05 14:31:14 +0900  update cli sabel entry point.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@554 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/sabel.php
+++ generator/skeleton/sabel.php
@@ -1,21 +1,54 @@
-#!/opt/local/php/bin/php
 <?php
-$s = microtime();
 
 $absolute_path = dirname(realpath(__FILE__));
+$tmp_path = explode('/', $absolute_path);
+array_pop($tmp_path);
+$absolute_path = join('/', $tmp_path);
 define('RUN_BASE', $absolute_path);
 
 $paths = explode(':', ini_get('include_path'));
 array_shift($paths);
 
 foreach ($paths as $path) {
-  if (is_dir($path . '/Sabel')) {
-    define('SABEL_USE_INCLUDE_PATH', true);
-  }
+  if (is_dir($path . '/Sabel')) define('SABEL_USE_INCLUDE_PATH', true);
 }
 
-require_once('lib/setup.php');
+require_once(RUN_BASE . '/config/environment.php');
+if (!defined('ENVIRONMENT')) {
+  // @todo this message to internationalization.
+  print "FATAL SABEL ERROR: you must define ENVIRONMENT in config/environment.php";
+  exit;
+}
 
-Sabel::main();
-$e = microtime();
-Sabel_Core_Context::log('total execution: '.($e - $s));
+require_once('Sabel/Container.php');
+$c = Container::create();
+
+$dt = new DirectoryTraverser();
+define('SABEL_CLASSES', RUN_BASE . '/cache/sabel_classes.php');
+if (ENVIRONMENT !== 'development' && is_readable(SABEL_CLASSES)) {
+  require_once(SABEL_CLASSES);
+  $dt->visit(new ClassRegister($c));
+  $dt->traverse();
+} else {
+  $dt->visit(new ClassCombinator(SABEL_CLASSES, null, false));
+  $dt->visit(new ClassRegister($c));
+  $dt->traverse();
+  require_once(SABEL_CLASSES);
+}
+unset($dt);
+
+$dt = new DirectoryTraverser(RUN_BASE);
+define('APP_CACHE', RUN_BASE.'/cache/app.php');
+if (ENVIRONMENT !== 'development' && is_readable(APP_CACHE)) {
+  require_once(APP_CACHE);
+  $dt->visit(new ClassRegister($c));
+  $dt->traverse();
+} else {
+  $dt->visit(new ClassCombinator(APP_CACHE, RUN_BASE, false, 'app'));
+  $dt->visit(new ClassRegister($c));
+  $dt->traverse();
+  require_once(APP_CACHE);
+}
+
+$frontController = $c->load('sabel.controller.Front');
+$frontController->ignition();
