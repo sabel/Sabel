360336718ecca2ada61e768c43c2e8f0bbd8903a
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-09-05 05:36:06 +0000

    r2820@Reos-MacBookPro:  morireo | 2006-09-05 14:36:01 +0900  sorry. previous commit has probelm.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@555 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/sabel.php
+++ generator/skeleton/sabel.php
@@ -1,16 +1,15 @@
 <?php
 
 $absolute_path = dirname(realpath(__FILE__));
-$tmp_path = explode('/', $absolute_path);
-array_pop($tmp_path);
-$absolute_path = join('/', $tmp_path);
 define('RUN_BASE', $absolute_path);
 
 $paths = explode(':', ini_get('include_path'));
 array_shift($paths);
 
 foreach ($paths as $path) {
-  if (is_dir($path . '/Sabel')) define('SABEL_USE_INCLUDE_PATH', true);
+  if (is_dir($path . '/Sabel')) {
+    define('SABEL_USE_INCLUDE_PATH', true);
+  }
 }
 
 require_once(RUN_BASE . '/config/environment.php');
