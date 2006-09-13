9ae9df74076f0969b91e92d7d52a58fa07f6f67c
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-09-13 14:15:10 +0000

    r2907@Reos-MacBookPro:  morireo | 2006-09-13 23:14:58 +0900  fix fatal problem
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@606 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/sabel.php
+++ generator/skeleton/sabel.php
@@ -11,7 +11,6 @@ foreach ($paths as $path) {
     define('SABEL_USE_INCLUDE_PATH', true);
   }
 }
-
 require_once(RUN_BASE . '/config/environment.php');
 if (!defined('ENVIRONMENT')) {
   // @todo this message to internationalization.
@@ -26,28 +25,39 @@ $dt = new DirectoryTraverser();
 define('SABEL_CLASSES', RUN_BASE . '/cache/sabel_classes.php');
 if (ENVIRONMENT !== 'development' && is_readable(SABEL_CLASSES)) {
   require_once(SABEL_CLASSES);
-  $dt->visit(new ClassRegister($c));
+  $dt->visit(new SabelClassRegister($c));
   $dt->traverse();
 } else {
   $dt->visit(new ClassCombinator(SABEL_CLASSES, null, false));
-  $dt->visit(new ClassRegister($c));
+  $dt->visit(new SabelClassRegister($c));
   $dt->traverse();
   require_once(SABEL_CLASSES);
 }
 unset($dt);
 
 $dt = new DirectoryTraverser(RUN_BASE);
-define('APP_CACHE', RUN_BASE.'/cache/app.php');
+define('APP_CACHE', RUN_BASE . '/cache/app.php');
+define('LIB_CACHE', RUN_BASE . '/cache/lib.php');
+define('SCM_CACHE', RUN_BASE . '/cache/schema.php');
+define('INJ_CACHE', RUN_BASE . '/cache/injection.php');
 if (ENVIRONMENT !== 'development' && is_readable(APP_CACHE)) {
   require_once(APP_CACHE);
-  $dt->visit(new ClassRegister($c));
+  require_once(LIB_CACHE);
+  require_once(SCM_CACHE);
+  require_once(INJ_CACHE);
+  $dt->visit(new AppClassRegister($c));
   $dt->traverse();
 } else {
   $dt->visit(new ClassCombinator(APP_CACHE, RUN_BASE, false, 'app'));
-  $dt->visit(new ClassRegister($c));
+  $dt->visit(new ClassCombinator(LIB_CACHE, RUN_BASE, false, 'lib'));
+  $dt->visit(new ClassCombinator(SCM_CACHE, RUN_BASE, false, 'schema'));
+  $dt->visit(new ClassCombinator(INJ_CACHE, RUN_BASE, false, 'injections'));
+  $dt->visit(new AppClassRegister($c));
   $dt->traverse();
   require_once(APP_CACHE);
+  require_once(LIB_CACHE);
+  require_once(SCM_CACHE);
+  require_once(INJ_CACHE);
 }
 
-$frontController = $c->load('sabel.controller.Front');
-$frontController->ignition();
+create('sabel.controller.Front')->ignition();
