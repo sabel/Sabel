0b27f39d33923321abe9c9c249a8d09afd1e51c7
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-09-29 08:38:04 +0000

    remove not sabel files
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@667 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/querys/autowrite.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE autowrite
-(
-  adstaff_id INT8 NOT NULL,
-  sites_id INT8 NOT NULL,
-  is_write boolean DEFAULT false
-);
-
-SET NAMES utf8;
--- generator/skeleton/querys/proxy.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE proxy
-(
-  id   int8 PRIMARY KEY AUTO_INCREMENT,
-  host varchar(64) UNIQUE NOT NULL,
-  rank char(4) NOT NULL,
-  port int2 NOT NULL
-);
