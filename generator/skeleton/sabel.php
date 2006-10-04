b8ebbeb5f1e4bbf63246e40cb7619286a7060486
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-04 08:11:38 +0000

    sabel.php copy index.php
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@683 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/sabel.php
+++ generator/skeleton/sabel.php
@@ -1,29 +1,7 @@
 <?php
 
-$absolute_path = dirname(realpath(__FILE__));
-define('RUN_BASE', $absolute_path);
-
-$paths = explode(':', ini_get('include_path'));
-array_shift($paths);
-
-foreach ($paths as $path) {
-  if (is_dir($path . '/Sabel')) {
-    define('SABEL_USE_INCLUDE_PATH', true);
-  }
-}
-
-require_once(RUN_BASE . '/config/environment.php');
-if (!defined('ENVIRONMENT')) {
-  // @todo this message to internationalization.
-  print "FATAL SABEL ERROR: you must define ENVIRONMENT in config/environment.php";
-  exit;
-}
-
-define('SABEL_CLASSES', RUN_BASE . '/cache/sabel_classes.php');
-define('APP_CACHE',     RUN_BASE . '/cache/app.php');
-define('LIB_CACHE',     RUN_BASE . '/cache/lib.php');
-define('SCM_CACHE',     RUN_BASE . '/cache/schema.php');
-define('INJ_CACHE',     RUN_BASE . '/cache/injection.php');
-
-require_once('Sabel/Container.php');
-Container::initializeApplication()->load('sabel.controller.Front')->ignition();
+require_once('setup.php');
+$container = Container::create();
+$front     = $container->load('sabel.controller.Front');
+$response  = $front->ignition();
+echo $response['html'];
