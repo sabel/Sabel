2caa9964b579e072d66da652c352e881e03f25b1
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-10 05:29:38 +0000

    refactoring.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@720 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/aspect/Calls.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Sabel_Aspect_Calls
+ *
+ * @category   Injection
+ * @package    org.sabel.injection
+ * @author     Mori Reo <mori.reo@gmail.com>
+ * @copyright  2002-2006 Mori Reo <mori.reo@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+class Sabel_Aspect_Calls
+{
+  private static $before = array();
+  private static $after  = array();
+  
+  /**
+   * add both before and after injection.
+   *
+   * @param InjectionCall object
+   * @return void
+   */
+  public static function add($injection)
+  {
+    if (!$injection) return false;
+    
+    $reflection = new ReflectionClass($injection);
+    foreach ($reflection->getMethods() as $method) {
+      if ($method->getName() === 'before') {
+        self::addBefore($injection);
+      } else if ($method->getName() === 'after') {
+        self::addAfter($injection);
+      }
+    }
+  }
+  
+  public static function doBefore($method, $arg, $reflection, $target)
+  {
+    $execute = true;
+    foreach (self::$before as $object) {
+      if ($object->when($method)) {
+        $execute = $object->before($method, $arg, $reflection, $target);
+      }
+    }
+    if (is_null($execute)) $execute = true;
+    return $execute;
+  }
+  
+  public static function doAfter($method, &$result, $reflection)
+  {
+    foreach (self::$after as $object) {
+      if ($object->when($method)) $object->after($method, $result, $reflection);
+    }
+  }
+  
+  public static function addBefore($injection)
+  {
+    self::$before[] = $injection;
+  }
+  
+  public static function addAfter($injection)
+  {
+    self::$after[] = $injection;
+  }
+}
---
+++ sabel/aspect/injections/Assign.php
@@ -0,0 +1,29 @@
+<?php
+
+class Sabel_Aspect_Injections_Assign
+{
+  public function when()
+  {
+    return true;
+  }
+  
+  public function after($method, $result, $reflection)
+  {
+    $anonr = Container::create()->instanciate('sabel.annotation.Reader');
+    $anonr->annotation($reflection->getName());
+    $assigns = $anonr->getAnnotationsByName($reflection->getName(), 'assign');
+    
+    $assignFromAnnotation = false;
+    foreach ($assigns as $annot) {
+      $assign = $annot->getContents();
+      if ($method->getName() === $assign[0]) {
+        $assignFromAnnotation = true;
+        Re::set($assign[2], $result);
+      }
+    }
+    
+    if (!$assignFromAnnotation) Re::set($method->getName(), $result);
+  }
+}
+
+Sabel_Aspect_Calls::add(new Sabel_Aspect_Injections_Assign());
