b6e674837955ef31f57b94c83f48e8d57b97ec80
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-16 14:25:59 +0000

    minor update. add VirtualInheritance
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@750 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ Test/VirtualInheritance.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Test_VirtualInheritance
+ * 
+ * @package org.sabel
+ * @author Mori Reo <mori.reo@gmail.com>
+ */
+class Test_VirtualInheritance extends SabelTestCase
+{
+  public static function suite()
+  {
+    return new PHPUnit2_Framework_TestSuite("Test_VirtualInheritance");
+  }
+  
+  public function __construct()
+  {
+    
+  }
+  
+  public function testVirtualInheritance()
+  {
+    $c = Container::create();
+    $c->regist('test.InheritedClass',        'InheritedClass');
+    $c->regist('test.VirtualParentClass',    'VirtualParentClass');
+    $c->regist('test.VirtualParentClassTwo', 'VirtualParentClassTwo');
+    
+    $inherited = $c->load('test.InheritedClass');
+    
+    $inherited->inherit('test.VirtualParentClass')
+              ->inherit('test.VirtualParentClassTwo');
+    
+    $this->assertEquals('test.Inherited',             $inherited->doSomething());
+    $this->assertEquals('test.VirtualParentClass',    $inherited->parentMethod());
+    $this->assertEquals('test.VirtualParentClassTwo', $inherited->parentMethodTwo());
+  }
+}
+
+class VirtualParentClass
+{
+  public function parentMethod()
+  {
+    return 'test.VirtualParentClass';
+  }
+}
+
+class VirtualParentClassTwo
+{
+  public function parentMethodTwo()
+  {
+    return 'test.VirtualParentClassTwo';
+  }
+}
+
+class InheritedClass
+{
+  public function doSomething()
+  {
+    return 'test.Inherited';
+  }
+}
