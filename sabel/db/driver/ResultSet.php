5f0155996044d3d818a453d216e004bed91db61b
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-18 06:31:20 +0000

    miss fix.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@757 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/db/driver/ResultSet.php
+++ sabel/db/driver/ResultSet.php
@@ -98,31 +98,3 @@ class Sabel_DB_Driver_ResultSet implements Iterator
     return ($this->pointer < sizeof($this->assocRow));
   }
 }
-
-class Sabel_DB_Driver_ResultObject
-{
-  private $data = array();
-
-  public function __construct($assocRow)
-  {
-    foreach ($assocRow as $column => $value) {
-      $this->data[$column] = $value;
-    }
-  }
-
-  public function __set($key, $val)
-  {
-    $this->data[$key] = $val;
-  }
-
-  public function __get($key)
-  {
-    $data = $this->data[$key];
-    return (is_numeric($data)) ? (int)$data : $data;
-  }
-
-  public function toArray()
-  {
-    return $this->data;
-  }
-}
