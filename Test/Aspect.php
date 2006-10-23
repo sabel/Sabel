a0beff9ecc6777ad4afba4f8286a9aeb4f354094
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-23 13:57:39 +0000

    minor update.
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@803 0a2fe512-f36d-da11-803c-00304871b6ac

--- Test/Aspect.php
+++ Test/Aspect.php
@@ -42,6 +42,200 @@ class Test_Aspect extends PHPUnit2_Framework_TestCase
     $ol->callOverloadsTwo('test');
     $this->assertEquals('callOverloadsTwo', $ol->afterResult('after'));
   }
+  
+  public function testIntertypeDeclaration()
+  {
+    $obj = new Test_Aspect_IntertypeDeclarator(new Test_Aspect_IntertypeTarget());
+    $this->assertEquals('test',   $obj->added());
+    $this->assertEquals('exists', $obj->exists());
+  }
+  
+  public function testPointcut()
+  {
+    $aspect    = new Test_AspectOne();
+    $aspectTwo = new Test_AspectTwo();
+    
+    $matcher = new Sabel_Aspect_Matcher();
+    $matcher->add($aspect->pointcut());
+    $matcher->add($aspectTwo->pointcut());
+    
+    $matches = $matcher->findMatch(array('class'=>'Target', 'method'=>'doSomething'));
+    $this->assertTrue($matches->matched('Test_AspectOne'));
+    $this->assertTrue($matches->matched('Test_AspectTwo'));
+    
+    $matches = $matcher->findMatch(array('class'=>'Target', 'method'=>'doWhat'));
+    $this->assertTrue($matches->matched('Test_AspectOne'));
+  }
+  
+  public function testAspect()
+  {
+    $aspects = Sabel_Aspect_Aspects::singleton();
+    $aspects->add(new Test_AspectOne());
+    $aspects->add(new Test_AspectTwo());
+    $maches = $aspects->findMatch(array('method' => 'doSome'));
+    $this->assertTrue($maches->matched('Test_AspectOne'));
+    $target = new Sabel_Aspect_Proxy(new Target());
+    $target->doSomething();
+  }
+  
+  public function testAspectAfterThrowing()
+  {
+    $aspects = Sabel_Aspect_Aspects::singleton();
+    $aspects->add(new ServerHandler());
+    
+    $rack = new Sabel_Aspect_Proxy(new Rack());
+    $firstMount  = $rack->mount(new Server('XEON'));
+    $secondMount = $rack->mount(new Server('Opteron'));
+    
+    $this->assertTrue($firstMount);
+    $this->assertEquals('rack space full', $secondMount);
+    $this->assertEquals(1, ServerStock::getNumberOfStock());
+    $this->assertEquals('Opteron', ServerStock::pop()->getName());
+  }
+}
+
+// test classes for testAspectAfterThrowing
+{
+  class NoRackSpaceException extends Exception { }
+  
+  class Rack
+  {
+    protected $space = 41;
+    
+    public function mount($server)
+    {
+      if ($this->space >= 42) {
+        throw new NoRackSpaceException('rack space full');
+      }
+      
+      $this->space++;
+      return true;
+    }
+  }
+  
+  class ServerStock
+  {
+    protected static $counter = 0;
+    protected static $stock = array();
+    
+    public static function add($server)
+    {
+      self::$counter++;
+      array_push(self::$stock, $server);
+    }
+    
+    public static function getNumberOfStock()
+    {
+      return self::$counter;
+    }
+    
+    public static function pop()
+    {
+      return array_pop(self::$stock);
+    }
+  }
+  
+  class Server
+  {
+    protected $name = '';
+    
+    public function __construct($name)
+    {
+      $this->name = $name;
+    }
+    
+    public function getName()
+    {
+      return $this->name;
+    }
+  }
+  
+  class ServerHandler
+  {
+    public function pointcut()
+    {
+      return Sabel_Aspect_Pointcut::create('ServerHandler', $this)->setMethodRegex('mount')
+                                                                  ->asAfter();
+    }
+    
+    public function afterThrowing(NoRackSpaceException $e, $arg, $target) {
+      ServerStock::add($arg[0]);
+      return $e->getMessage();
+    }
+  }
+}
+
+
+class Target
+{
+  public function doSomething()
+  {
+    
+  }
+}
+
+class Test_AspectOne
+{
+  public function pointcut()
+  {
+    return Sabel_Aspect_Pointcut::create('Test_AspectOne', $this)->setMethodRegex('do.*')
+                                                                 ->asAfter();
+  }
+  
+  public function around()
+  {
+    
+  }
+  
+  public function before()
+  {
+    
+  }
+  
+  public function after()
+  {
+    
+  }
+}
+
+class Test_AspectTwo
+{
+  public function pointcut()
+  {
+    return Sabel_Aspect_Pointcut::create('Test_AspectTwo', $this)->setMethodRegex('doS.*')
+                                                                 ->asBefore();
+  }
+  
+  public function around()
+  {
+    
+  }
+  
+  public function before()
+  {
+    
+  }
+  
+  public function after()
+  {
+    
+  }
+}
+
+class Test_Aspect_IntertypeDeclarator extends Sabel_Aspect_DynamicProxy
+{
+  public function added()
+  {
+    return 'test';
+  }
+}
+
+class Test_Aspect_IntertypeTarget
+{
+  public function exists()
+  {
+    return 'exists';
+  }
 }
 
 class Test_Aspect_Customers
