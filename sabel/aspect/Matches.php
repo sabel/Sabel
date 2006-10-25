8f003168b4ceb7cb821bc6295663cdc2e39ef283
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-25 08:39:52 +0000

    add matches
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@817 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/aspect/Matches.php
@@ -0,0 +1,47 @@
+<?php
+
+class Sabel_Aspect_Matches implements Iterator
+{
+  protected $size        = 0;
+  protected $position    = 0;
+  protected $matches     = array();
+  protected $matchesList = array();
+  
+  public function add($name, $aspect)
+  {
+    $this->matchesList[$name] = true;
+    $this->matches[]          = $aspect;
+  }
+  
+  public function matched($name)
+  {
+    return (isset($this->matchesList[$name]));
+  }
+  
+  public function current()
+  {
+    $matches = $this->matches;
+    return $matches[$this->position];
+  }
+  
+  public function rewind()
+  {
+    $this->position = 0;
+    $this->size = count($this->matches);
+  }
+  
+  public function valid()
+  {
+    return ($this->position < $this->size);
+  }
+  
+  public function next()
+  {
+    $this->position++;
+  }
+  
+  public function key()
+  {
+    return $this->position;
+  }
+}
