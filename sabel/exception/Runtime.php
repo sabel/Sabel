d3d69dd081d7d096ce3e02197699d06dc4836733
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-27 14:18:03 +0000

    comment
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@839 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/exception/Runtime.php
+++ sabel/exception/Runtime.php
@@ -1,5 +1,86 @@
 <?php
 
+/**
+ * Sabel_Exception_Runtime
+ *
+ * @category   Exception
+ * @package    org.sabel.exception
+ * @author     Mori Reo <mori.reo@gmail.com>
+ * @copyright  2002-2006 Mori Reo <mori.reo@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
 class Sabel_Exception_Runtime extends Exception
 {
+  public function flightLog()
+  {
+    $file = $this->getFile();
+    $code = $this->getCode();
+    $line = $this->getLine();
+    $msg  = $this->getMessage();
+    $traceRaw = $this->getTrace();
+    
+    for ($i = 0, $c = count($traceRaw); $i < $c; $i++) {
+      if (!isset($traceRaw[$i]['file'])) continue;
+      
+      $stFile = $traceRaw[$i]['file'];
+      $stLine = $traceRaw[$i]['line'];
+      $stFunction = $traceRaw[$i]['function'];
+      
+      $trace[] = sprintf('at %s in [%s:%s]', $stFunction, $stFile, $stLine);
+    }
+    
+    $format = 'Exception: %s from "%s" on "%s"' . "\n";
+    $errorMsg = sprintf($format, $msg, $file, $line);
+    
+    foreach ($trace as $l) {
+      $errorMsg .= $l . "\n";
+    }
+    
+    $time = date('D d H:i:s');
+    if (!$this->writeFile($time.': '.$errorMsg)) $this->writeSyslog($errorMsg);
+    exit;
+  }
+  
+  protected function writeFile($msg)
+  {
+    if (!$fp = @fopen(SabelConst::EXCEPTION_LOG_FILE_NAME, 'a')) return false;
+    fwrite($fp, $msg);
+    fclose($fp);
+    return true;
+  }
+  
+  protected function writeSyslog($msg)
+  {
+    openlog('SabelErrorLog', LOG_PID | LOG_ERROR, LOG_LOCAL0);
+    syslog(LOG_WARNING, $msg);
+    closelog();
+  }
+  
+  public function printStackTrace()
+  {
+    $file = $this->getFile();
+    $code = $this->getCode();
+    $line = $this->getLine();
+    $msg  = $this->getMessage();
+    $traceRaw = $this->getTrace();
+    
+    for ($i = 0, $c = count($traceRaw); $i < $c; $i++) {
+      if (!isset($traceRaw[$i]['file'])) continue;
+      
+      $stFile = $traceRaw[$i]['file'];
+      $stLine = $traceRaw[$i]['line'];
+      $stFunction = $traceRaw[$i]['function'];
+      
+      $trace[] = sprintf('at %s in [%s:%s]', $stFunction, $stFile, $stLine);
+    }
+    
+    $format = 'Exception: %s from "%s" on "%s"' . "<br/><br/>\n\n";
+    $errorMsg = sprintf($format, $msg, $file, $line);
+    
+    foreach ($trace as $l) {
+      $errorMsg .= $l . "<br/>\n";
+    }
+    
+    print $errorMsg;
+  }
 }
