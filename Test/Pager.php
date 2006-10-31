3b21a6b86e11c1eb16e4f435f21e1741988dc2ad
hamanaka <hamanaka@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-10-31 12:13:21 +0000

    pager class add
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@844 0a2fe512-f36d-da11-803c-00304871b6ac

--- Test/Pager.php
+++ Test/Pager.php
@@ -9,24 +9,110 @@ require_once('PHPUnit2/Framework/TestCase.php');
  */
 class Test_Pager extends PHPUnit2_Framework_TestCase
 {
+  private $pager = null;
+  
   public static function suite()
   {
     return new PHPUnit2_Framework_TestSuite("Test_Pager");
   }
   
+  public function setUp()
+  {
+    $this->pager = Sabel_View_Pager::create();
+  }
+  
   public function testStandardPagerUse()
   {
+    $pager = clone $this->pager;
+    
+    $pager->setNumberOfItem(100);
+    $pager->setLimit(10);
+    $pager->setPageNumber(3);
+    
+    $this->assertEquals(100, $pager->getNumberOfItem());
+    $this->assertEquals(10,  $pager->getLimit());
+    $this->assertEquals(3,   $pager->getPageNumber());
+    $this->assertEquals(20,  $pager->getSqlOffset());
   }
   
-  public function testExtendedPagerUse()
+  public function testSetterAndGetterPagerUse()
   {
+    $pager = clone $this->pager;
+    
+    $pager->numberOfItem = 100;
+    $pager->limit = 10;
+    $pager->pageNumber = 3;
+    
+    $this->assertEquals(100, $pager->numberOfItem);
+    $this->assertEquals(10,  $pager->limit);
+    $this->assertEquals(3,   $pager->pageNumber);
+    $this->assertEquals(20,  $pager->sqlOffset);
   }
   
-  public function testControllerSimuration()
+  public function testPageNumberRoundPagerUse()
   {
+    $pager = clone $this->pager;
+    
+    $pager->setNumberOfItem(300);
+    $pager->setLimit(40);
+    $pager->setPageNumber(100);
+    
+    $this->assertEquals(300, $pager->getNumberOfItem());
+    $this->assertEquals(40,  $pager->getLimit());
+    $this->assertEquals(8,   $pager->getPageNumber());
+    $this->assertEquals(280, $pager->getSqlOffset());
   }
   
-  public function testExceptedUse()
+  public function testExceptedPagerUse()
   {
+    $pager = clone $this->pager;
+    
+    try {
+      $pager->setNumberOfItem(-1);
+      $this->fail('set number of item method not thrown.');
+    } catch (Sabel_Exception_Runtime $e) {}
+    
+    try {
+      $pager->setNumberOfItem('a');
+      $this->fail('set number of item method not thrown.');
+    } catch (Sabel_Exception_Runtime $e) {}
+    
+    $pager->setLimit('a');
+    $pager->setPageNumber('a');
+    
+    $this->assertEquals(0, $pager->getNumberOfItem());
+    $this->assertEquals(1, $pager->getLimit());
+    $this->assertEquals(1, $pager->getPageNumber());
+    $this->assertEquals(0, $pager->getSqlOffset());
+  }
+  
+  public function testUnusualPagerUse()
+  {
+    $pager = clone $this->pager;
+    
+    $pager->setPageNumber(10);
+    
+    $this->assertEquals(1, $pager->getPageNumber());
+    $this->assertEquals(0, $pager->getSqlOffset());
+    
+    $pager->setNumberOfItem(250);
+    $pager->setLimit(15);
+    
+    $this->assertEquals(250, $pager->getNumberOfItem());
+    $this->assertEquals(15,  $pager->getLimit());
+    $this->assertEquals(10,  $pager->getPageNumber());
+    $this->assertEquals(135, $pager->getSqlOffset());
+  }
+  
+  public function testInitializedPagerUse()
+  {
+    $pager = Sabel_View_Pager::create(200, 20);
+    
+    $pager->setPageNumber(4.3);
+    
+    $this->assertEquals(200, $pager->getNumberOfItem());
+    $this->assertEquals(20,  $pager->getLimit());
+    $this->assertEquals(4,   $pager->getPageNumber());
+    $this->assertEquals(60,  $pager->getSqlOffset());
   }
 }
--- sabel/view/Pager.php
+++ sabel/view/Pager.php
@@ -8,32 +8,78 @@
  */
 class Sabel_View_Pager
 {
-  public static function getCount()
+  private static $instance = null;
+  
+  private $pageItem     = 25;
+  private $pageNumber   = 0;
+  private $numberOfItem = 0;
+  
+  private function __construct($count, $limit)
   {
+    if (isset($count)) $this->setNumberOfItem($count);
+    if (isset($limit)) $this->setLimit($limit);
+  }
+  
+  public static function create($count = null, $limit = null)
+  {
+    if (is_null(self::$instance) || isset($count, $limit))
+      self::$instance = new self($count, $limit);
+    return self::$instance;
+  }
+  
+  public function __set($key, $value)
+  {
+    $method = 'set' . ucfirst($key);
+    if (method_exists($this, $method)) $this->$method($value);
+  }
+  
+  public function __get($key)
+  {
+    $method = 'get' . ucfirst($key);
+    if (method_exists($this, $method)) return $this->$method();
+  }
+  
+  public function __call($key, $args)
+  {
+    $method = 'set' . ucfirst($key);
+    if (method_exists($this, $method)) $this->$method($args[0]);
+  }
+  
+  public function setNumberOfItem($item)
+  {
+    if ($item < 0 || !is_numeric($item))
+      throw new Sabel_Exception_Runtime('invalid number of item : ' . $item);
     
+    $this->numberOfItem =(int) $item;
   }
   
-  public static function setCount($count)
+  public function getNumberOfItem()
   {
-    
+    return $this->numberOfItem;
   }
   
-  public static function setLimit($limit)
+  public function setLimit($limit)
   {
-    
+    $this->pageItem =(int) max($limit, 1);
   }
   
-  public static function getLimit()
+  public function getLimit()
   {
-    return 100;
+    return $this->pageItem;
   }
   
-  public static function setOffset($offset)
+  public function setPageNumber($page)
   {
+    $this->pageNumber =(int) max($page, 1);
   }
   
-  public static function getOffset()
+  public function getPageNumber()
   {
-    return 0;
+    return (int) round(max(min(($this->numberOfItem / $this->pageItem), $this->pageNumber), 1));
+  }
+  
+  public function getSqlOffset()
+  {
+    return (int) floor($this->pageItem * ($this->getPageNumber() - 1));
   }
 }
