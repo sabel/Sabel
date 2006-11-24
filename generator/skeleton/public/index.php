148d7c5bacb54196f7aac67c28828f887391d88b
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-11-24 06:53:10 +0000

    update skeleton
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@933 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/public/index.php
+++ generator/skeleton/public/index.php
@@ -1,7 +1,22 @@
 <?php
 
-require_once('../setup.php');
-$container = Container::create();
-$front     = $container->load('sabel.controller.Front');
-$response  = $front->ignition();
-echo $response['html'];
+function _($val)
+{
+  return $val;
+}
+
+ob_start();
+
+require ('../config/environment.php');
+require ('../setup.php');
+require ('Sabel/Sabel.php');
+Sabel::initializeApplication();
+require (APP_CACHE);
+require (LIB_CACHE);
+require (SCM_CACHE);
+require (INJ_CACHE);
+
+$front = new Sabel_Controller_Front();
+$response = $front->ignition();
+
+ob_flush();
