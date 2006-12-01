c71c92d80015020c560d9394084a0e7cdb934daa
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-12-01 15:27:39 +0000

    update
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@985 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/Request.php
+++ sabel/Request.php
@@ -137,3 +137,56 @@ class Sabel_Request
     return $this->requestUri;
   }
 }
+
+class Sanitize
+{
+  /**
+   * remove non alphabet and non numeric characers.
+   *
+   * @param mixed  $target string or array
+   * @param string $expected
+   * @return mixed cleaned string or array
+   */
+  public static function removeNonAlphaNumeric(&$target, $expected = '')
+  {
+    $cleaned = null;
+
+    if(is_array($target)) {
+      foreach ($target as $key => $value) {
+        $cleaned[$key] = preg_replace( "/[^${$expected}a-zA-Z0-9]/", '', $value);
+      }
+    } else {
+      $cleaned = preg_replace( "/[^${$expected}a-zA-Z0-9]/", '', $target);
+    }
+
+    return $cleaned;
+  }
+
+  /**
+   * target SQL string to make SQL safety
+   *
+   */
+  public static function sqlSafe($target)
+  {
+    return addslashes($target);
+  }
+
+  public static function normalize(&$target)
+  {
+    $cleaned = null;
+
+    if (get_magic_quotes_gpc()) {
+      if (is_array($target)) {
+        foreach ($target as $key => $value) {
+          $cleaned[$key] = stripslashes($value);
+        }
+      } else {
+        $cleaned = stripslashes($target);
+      }
+    } else {
+      $cleaned = $target;
+    }
+
+    return $cleaned;
+  }
+}
