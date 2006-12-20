0a71ff5161bf2c0e12936144ccc5f11c300b9fa4
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-12-20 15:00:54 +0000

    change author
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1145 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/db/Model.php
+++ sabel/db/Model.php
@@ -9,8 +9,8 @@ Sabel::using('Sabel_DB_SimpleCache');
  *
  * @category   DB
  * @package    org.sabel.db
- * @author     Mori Reo <mori.reo@gmail.com>
- * @copyright  2002-2006 Mori Reo <mori.reo@gmail.com>
+ * @author     Ebine Yutaka <ebine.yutaka@gmail.com>
+ * @copyright  2002-2006 Ebine Yutaka <ebine.yutaka@gmail.com>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  */
 class Sabel_DB_Model extends Sabel_DB_Executer
