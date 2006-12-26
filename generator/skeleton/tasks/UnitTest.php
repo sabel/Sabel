a1c2a0428b0fcd5da00dd4d72ae425d73cad18da
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2006-12-26 09:12:08 +0000

    r1213@Reos-Computer:  morireo | 2006-12-26 18:09:30 +0900  add UnitTest task
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1186 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ generator/skeleton/tasks/UnitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+define("RUN_BASE", getcwd());
+add_include_path("/tests");
+
+define('TEST',        0x05);
+define('ENVIRONMENT', TEST);
+
+Sabel::fileUsing("config/environment.php");
+Sabel::fileUsing("config/database.php");
+Sabel::fileUsing("config/connection_map.php");
+
+Sabel::using('Sabel_DB_Connection');
+Sabel::using('Sabel_DB_Executer');
+Sabel::using('Sabel_DB_Model');
+
+Sabel::using("Sabel_Test_Model");
+Sabel::using("Sabel_Test_ModelRunner");
+
+/**
+ * Migration
+ *
+ * @author     Mori Reo <mori.reo@gmail.com>
+ * @copyright  2002-2006 Mori Reo <mori.reo@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+class UnitTest extends Sakle
+{
+  public function execute()
+  {
+    if (!isset($this->arguments[1])) {
+      throw new Exception("model name must be specified");
+    }
+    
+    Sabel_Test_ModelRunner::create()->start($this->arguments[1]);
+  }
+}
