ec08f082650a8b9c42451fece8c1320b337a2f93
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-01-22 05:55:43 +0000

    r3843@Reos-Computer:  morireo | 2007-01-22 14:55:28 +0900  add Exception Handler
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1267 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/controller/plugin/ExceptionHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+class Sabel_Controller_Plugin_ExceptionHandler implements Sabel_Controller_Page_Plugin
+{
+  public function onException($controller, $exception)
+  {
+    $c = Sabel::load("Common_ExceptionHandler");
+    $c->setup($controller->getRequest());
+    $c->initialize();
+    $ref = new ReflectionClass($exception);
+    $className = str_replace("Exception_", "", $ref->getName());
+    $c->$className($exception);
+  }
+  
+  public function onBeforeAction($controller) {}
+  public function onAfterAction($controller) {}
+  public function onRedirect($controller) {}
+}
