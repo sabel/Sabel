918f90a1af46b71848d474d65f8b365b0f71e240
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-03-11 12:43:37 +0000

    r2309@morireos-computer:  morireo | 2007-03-11 21:42:44 +0900  view performance improvement
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1372 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/view/Renderer.php
+++ sabel/view/Renderer.php
@@ -19,13 +19,42 @@ abstract class Sabel_View_Renderer
   
   abstract public function rendering($path, $name, $values);
   
-  public function partial($templateName, $values = array())
+  public function partial($template_name, $options = array())
   {
     $v = new Sabel_View();
-    $v->assignByArray($values);
-    $v->decideTemplatePath(Sabel_Context::getCurrentCandidate(), true);
-    $v->setTemplateName($templateName);
+    
+    if (isset($options["values"])) {
+      $v->assignByArray(array());
+    }
+    
+    if (is_readable(RUN_BASE . "/app/views/" . $template_name)) {
+      $v->setTemplatePath(RUN_BASE . "/app/views/");
+    } else {
+      $v->decideTemplatePath(Sabel_Context::getCurrentCandidate(), true);
+    }
+    
+    $v->setTemplateName($template_name);
     if ($v->isTemplateMissing()) throw new Exception('Template file is not found');
-    return $v->rendering(false);
+    
+    if (isset($options["cache"]) && $options["cache"] === true) {
+      $key = "";
+      
+      if (isset($options["key"])) {
+        $key = $options["key"];
+      }
+      
+      $cpath = RUN_BASE . "/cache/" . $template_name . $key;
+      
+      if (is_readable($cpath)) {
+        $partial_html = file_get_contents($cpath);
+      } else {
+        $partial_html = $v->rendering(false);
+        file_put_contents($cpath, $partial_html);
+      }
+    } else {
+      $partial_html = $v->rendering(false);
+    }
+    
+    return $partial_html;
   }
 }
