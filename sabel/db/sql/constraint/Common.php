a6c727cc8e12e102356c4ae680e78e8d8d7b2edd
svnebine <svnebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-04-22 16:39:21 +0000

    spell miss fix
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1424 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/db/sql/constraint/Common.php
+++ sabel/db/sql/constraint/Common.php
@@ -20,7 +20,7 @@ class Sabel_DB_Sql_Constraint_Common implements Sabel_DB_Sql_Constraint_Interfac
     if (isset($c["order"]))  $sql .= " ORDER BY " . $c["order"];
 
     if (isset($c["offset"]) && !isset($c["limit"])) {
-      $sql .= " LIMIT 100 OFFSEt " . $c["offset"];
+      $sql .= " LIMIT 100 OFFSET " . $c["offset"];
     } else {
       if (isset($c["limit"]))  $sql .= " LIMIT "    . $c["limit"];
       if (isset($c["offset"])) $sql .= " OFFSET "   . $c["offset"];
