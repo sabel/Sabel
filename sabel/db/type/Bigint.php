a8aab8a8ccc6673f5128da0d4fd549bc42c46f1d
svnebine <svnebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-05-07 08:37:44 +0000

    add Bigint.php
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1439 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ sabel/db/type/Bigint.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Sabel_DB_Type_Bigint
+ *
+ * @category   DB
+ * @package    org.sabel.db
+ * @author     Ebine Yutaka <ebine.yutaka@gmail.com>
+ * @copyright  2002-2006 Ebine Yutaka <ebine.yutaka@gmail.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ */
+class Sabel_DB_Type_Bigint implements Sabel_DB_Type_Interface
+{
+  private $next = null;
+
+  public function getType()
+  {
+    return Sabel_DB_Type::BIGINT;
+  }
+
+  public function add($chain)
+  {
+    $this->next = $chain;
+  }
+
+  public function send($co, $type)
+  {
+    $types = array("bigint", "int8", "bigserial");
+
+    if (in_array($type, $types)) {
+      $co->type = $this->getType();
+      $co->max  =  9223372036854775807;
+      $co->min  = -9223372036854775808;
+    } else {
+      $this->next->send($co, $type);
+    }
+  }
+}
