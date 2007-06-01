9c8d35632c86bffa40273c404bec28c5196565a6
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-06-01 15:49:35 +0000

    updates skeleton
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1520 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/config/Factory.php
+++ generator/skeleton/config/Factory.php
@@ -7,6 +7,8 @@ class Factory extends Sabel_Container_Injection
     $this->bind("Sabel_Request")->to("Sabel_Request_Web");
     
     $this->bind("Sabel_Controller_Executer")
-          ->to("Sabel_Controller_Executer_Flow");
+          ->to("Sabel_Controller_Executer_Basic");
+    
+    $this->bind("Sabel_Response")->to("Sabel_Response_Web");
   }
 }
