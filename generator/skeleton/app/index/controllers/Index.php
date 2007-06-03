8ffb45a61eb56a38961500eee7e924f4f3b8bb3f
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-06-03 07:05:58 +0000

    update documents and delete useless skeleton and update
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1534 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/app/index/controllers/Index.php
+++ generator/skeleton/app/index/controllers/Index.php
@@ -2,12 +2,4 @@
 
 class Index_Controllers_Index extends Sabel_Controller_Page
 {
-  public function index()
-  {
-  }
-  
-  public function notfound()
-  {
-    echo 'page notfound';
-  }
 }
--- generator/skeleton/config/Map.php
+++ generator/skeleton/config/Map.php
@@ -5,10 +5,9 @@ class Map extends Sabel_Map_Config
   public function configure()
   {
     $this->route("default")
-           ->uri(":controller/:action/:id")
+           ->uri(":controller/:action")
            ->module("index")
            ->defaults(array(":controller" => "index",
-                            ":action"     => "index",
-                            ":id"         => null));
+                            ":action"     => "index"));
   }
 }
