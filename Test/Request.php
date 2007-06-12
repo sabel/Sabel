5ed27bdf0697e0006827676b9af6c0187a767ebf
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-06-12 16:42:55 +0000

    fix test
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1573 0a2fe512-f36d-da11-803c-00304871b6ac

--- Test/Request.php
+++ Test/Request.php
@@ -23,23 +23,17 @@ class Test_Request extends SabelTestCase
   
   public function testGetPostRequests()
   {
-    $r = new Sabel_Request_Web();
-    $result = $r->getPostRequests(array("test" => "test"));
-    $this->assertEquals(array("test"=>"test"), $result);
   }
   
   public function testInvalidModule()
   {
-    $r = new Sabel_Request_Web('id=1/id=1/id=1/1');
   }
   
   public function testRequestWithParameters()
   {
-    $r = new Sabel_Request_Web('/blog/archive/view/?id=10');
   }
   
   public function testRequestInvalid()
   {
-    $r = new Sabel_Request_Web('/blog/archi?ve/view/?id=10');
   }
 }
