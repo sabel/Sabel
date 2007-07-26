e5cd36cb1c7f6682f024a246285d33fd954c0ba5
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-07-26 11:11:41 +0000

    minor update in generator
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1738 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/generator.php
+++ generator/generator.php
@@ -1,29 +1,41 @@
 <?php
 
-require_once ('Sabel/Sabel.php');
-require_once ('classes.php');
+require_once ("Sabel/Sabel.php");
+require_once ("classes.php");
 
-$args = $_SERVER['argv'];
+$args = $_SERVER["argv"];
 
-define(RUN_BASE, getcwd());
+if (isset($args[1])) {
+  if (strpos($args[1], "-") === false) {
+    $dir = getcwd() . DIR_DIVIDER . $args[1];
+  }
+} else {
+  $dir = getcwd();
+}
+
+define(RUN_BASE, $dir);
+
+if (!is_dir(RUN_BASE)) {
+  mkdir(RUN_BASE);
+}
 
 $pathToSabel = dirname(dirname($args[0]));
 $includePath = get_include_path();
-if (!in_array($pathToSabel, explode(':', $includePath))) {
-  set_include_path(get_include_path().':'.$pathToSabel);
+if (!in_array($pathToSabel, explode(":", $includePath))) {
+  set_include_path(get_include_path().":".$pathToSabel);
 }
 
-$dt = new Sabel_Util_DirectoryTraverser(dirname(__FILE__) . '/skeleton');
+$dt = new Sabel_Util_DirectoryTraverser(dirname(__FILE__) . "/skeleton");
 $aCreator = new SabelDirectoryAndFileCreator();
 
 for ($i = 0, $count = count($args); $i < $count; ++$i) {
-  if ($args[$i] === '--overwrite') {
+  if ($args[$i] === "--overwrite") {
     $aCreator->setOverwrite(true);
-  } elseif ($args[$i] === '--ignore') {
+  } elseif ($args[$i] === "--ignore") {
     $fwArg = $args[$i+1];
-    if (isset($fwArg) && stripos($fwArg, '--') === false) {
-      if (stripos($fwArg, ',') !== false) {
-        $ignores = explode(',', $fwArg);
+    if (isset($fwArg) && stripos($fwArg, "--") === false) {
+      if (stripos($fwArg, ",") !== false) {
+        $ignores = explode(",", $fwArg);
         foreach ($ignores as $ignore) {
           $aCreator->addIgnore($ignore);
         }
