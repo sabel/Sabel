31c0e522ab409ff92ecc1995cea8468648f9ab4a
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-08-21 14:11:10 +0000

    update skeleton
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1783 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/config/Bus.php
+++ generator/skeleton/config/Bus.php
@@ -5,7 +5,7 @@ class Config_Bus extends Sabel_Bus_Config
   public function configure()
   {
     $this->addAsGroup(new Sabel_Processor_Request("request"));
-    $this->addAsGroup(new Sabel_Processor_I18n("i18n"));
+    // $this->addAsGroup(new Sabel_Processor_I18n("i18n"));
     $this->addAsGroup(new Sabel_Processor_Router("router"));
     $this->addAsGroup(new Sabel_Processor_Helper("helper"));
     $this->addAsGroup(new Sabel_Processor_Creator("creator"));
@@ -23,8 +23,8 @@ class Config_Bus extends Sabel_Bus_Config
     $errors = new Processor_Errors("errors");
     $this->get("executer")->get("executer")->insertNext($errors);
     
-    $acl = new Processor_Acl("acl");
-    $this->get("executer")->get("executer")->getFirst()->insertPrevious($acl);
+    // $acl = new Processor_Acl("acl");
+    // $this->get("executer")->get("executer")->getFirst()->insertPrevious($acl);
     
     return $this;
   }
