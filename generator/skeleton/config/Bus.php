94dc4d25e1a17cf71e9474861cbb6bb049ce2541
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-09-12 12:26:36 +0000

    update config/Bus.php of skeleton
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1852 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/config/Bus.php
+++ generator/skeleton/config/Bus.php
@@ -4,12 +4,12 @@ class Config_Bus extends Sabel_Bus_Config
 {
   public function configure()
   {
-    // $this->add(new Processor_Selecter("selecter"));
     $this->add(new Processor_Request("request"));
     $this->add(new Processor_Router("router"));
     // $this->add(new Processor_I18n("i18n"));
     $this->add(new Processor_Helper("helper"));
     $this->add(new Processor_Creator("creator"));
+    // $this->add(new Processor_Selecter("selecter"));
     $this->add(new Processor_Initializer("initializer"));
     $this->add(new Processor_Redirecter("redirecter"));
     // $this->add(new Processor_Acl("acl"));
