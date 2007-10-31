ec37cfd4ed682a1769a1aa4bdc50e1976d09dcf6
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-10-31 09:24:44 +0000

    add addon to Config_Bus
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@1949 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/config/Bus.php
+++ generator/skeleton/config/Bus.php
@@ -4,17 +4,13 @@ class Config_Bus extends Sabel_Bus_Config
 {
   public function configure()
   {
+    $this->add(new Processor_Addon("addon"));
     $this->add(new Processor_Request("request"));
     $this->add(new Processor_Router("router"));
     $this->add(new Processor_Location("location"));
-    // $this->add(new Processor_I18n("i18n"));
-    $this->add(new Processor_Helper("helper"));
     $this->add(new Processor_Creator("creator"));
-    // $this->add(new Processor_Selecter("selecter"));
     $this->add(new Processor_Initializer("initializer"));
     $this->add(new Processor_Redirecter("redirecter"));
-    $this->add(new Processor_Form("form"));
-    // $this->add(new Processor_Acl("acl"));
     $this->add(new Processor_Executer("executer"));
     $this->add(new Processor_Response("response"));
     $this->add(new Processor_Renderer("renderer"));
