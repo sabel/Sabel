384d67e5c9fe7a74d9904b2d51557793285eabd4
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2007-12-12 06:11:26 +0000

    fix windows batch files
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2003 0a2fe512-f36d-da11-803c-00304871b6ac

--- bin/sakle.bat
+++ bin/sakle.bat
@@ -3,6 +3,7 @@
 if "%OS%"=="Windows_NT" @setlocal
 
 set SCRIPT_DIR=%~dp0
+set SABEL=c:\php\includes\Sabel
 
 goto init
 
@@ -10,7 +11,7 @@ goto init
 
 if "%PHP_COMMAND%" == "" goto no_phpcommand
 
-%PHP_COMMAND% -d html_errors=off -d open_basedir= -q "%SCRIPT_DIR%\..\sabel\Sakle.php" %1 %2 %3 %4 %5 %6 %7 %8 %9
+%PHP_COMMAND% -d html_errors=off -d open_basedir= -q "%SABEL%\sabel\Sakle.php" %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 goto cleanup
 
