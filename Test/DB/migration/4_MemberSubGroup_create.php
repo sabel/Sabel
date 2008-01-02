2e5e004ab25469ccee7a71fbe384ae3cd815a08a
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2008-01-02 14:02:44 +0000

    delete old migration files for test
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2033 0a2fe512-f36d-da11-803c-00304871b6ac

--- Test/DB/migration/4_MemberSubGroup_create.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$create->column("id")->type(_INT)->nullable(false)->primary(true)->increment(false)->value(_NULL);
-$create->column("member_group_id")->type(_INT)->nullable(false)->primary(false)->increment(false)->value(_NULL);
-$create->column("name")->type(_STRING)->nullable(false)->primary(false)->increment(false)->value(_NULL)->length(32);
-
-$create->fkey("member_group_id")->table("member_group")->column("id")->onDelete("NO ACTION")->onUpdate("NO ACTION");
-$create->options("engine", "InnoDB");
