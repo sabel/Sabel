028202f8af049bd30ae0301a23696003ba71595a
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2008-03-14 16:42:53 +0000

    delete old admin module
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2173 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/config/Map.php
+++ generator/skeleton/config/Map.php
@@ -4,15 +4,6 @@ class Config_Map extends Sabel_Map_Configurator
 {
   public function configure()
   {
-    if (ENVIRONMENT !== PRODUCTION) {
-      $this->route("devel")
-             ->uri("devel/:controller/:action/:param")
-             ->module("devel")
-             ->defaults(array(":controller" => "main",
-                              ":action"     => "index",
-                              ":param"      => null));
-    }
-    
     $this->route("default")
            ->uri(":controller/:action")
            ->module("index")
