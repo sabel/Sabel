a323429a519909771794365182eee9f9878e029f
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2008-04-27 11:10:21 +0000

    fixed migration
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2324 0a2fe512-f36d-da11-803c-00304871b6ac

---
+++ Test/DB/migration/create.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sakle Migration development schema_test head
+sakle Migration development student head
+sakle Migration development student_course head
+sakle Migration development course head
+sakle Migration development session head
+sakle Migration development sbl_storage head
+sakle Migration development grandparents head
+sakle Migration development parents head
+sakle Migration development children head
+sakle Migration development grandchildren head
