b0e77c2af67015f739b6cce90abcbbb34d65f762
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2008-04-28 15:45:52 +0000

    updates Sabel.Mail
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2334 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/mail/sender/Smtp.php
+++ sabel/mail/sender/Smtp.php
@@ -22,6 +22,11 @@ class Sabel_Mail_Sender_Smtp
    */
   protected $port = "25";
   
+  /**
+   * @var resource
+   */
+  protected $smtp = null;
+  
   public function __construct($server = "localhost", $port = "25")
   {
     $this->server = $server;
@@ -42,28 +47,104 @@ class Sabel_Mail_Sender_Smtp
       $headers["MIME-Version"] = "1.0";
     }
     
-    $smtp = fsockopen($this->server, $this->port);
+    $this->connect();
     
-    fputs($smtp,"HELO $server\r\n");
-    fputs($smtp,"MAIL FROM:<{$headers["From"]}>\r\n");
-    fputs($smtp,"RCPT TO:<$rcptTo>\r\n");
-    fputs($smtp, "DATA\r\n");
+    $this->command("EHLO {$this->server}");
+    while ($result = trim(fgets($this->smtp))) {
+      if (strpos($result, "250") !== 0) return false;
+      if (strpos($result, "250 ") === 0) break;
+    }
+    
+    if ($result === false) return false;
+    
+    if (isset($options["auth"])) {
+      $method = strtolower($options["auth"]) . "Auth";
+      $this->$method($options["user"], $options["password"]);
+    }
+    
+    $this->command("MAIL FROM:<{$headers["From"]}>", "250");
+    
+    try {
+      $this->command("RCPT TO:<{$rcptTo}>", "250");
+    } catch (Sabel_Mail_Smtp_Exception $e) {
+      $exception = new Sabel_Mail_Smtp_Exception_RecipientRefused($e->getMessage());
+      $exception->setResponseCode($e->getResponseCode());
+      throw $exception;
+    }
+
+    $this->command("DATA", "354");
     
     foreach ($headers as $name => $header) {
       if (is_array($header)) {
         foreach ($header as $value) {
-          fputs($smtp, "{$name}: {$value}\r\n");
+          $this->command("{$name}: {$value}");
         }
       } else {
-        fputs($smtp, "{$name}: {$header}\r\n");
+        $this->command("{$name}: {$header}");
       }
     }
     
-    fputs($smtp, "\r\n$body\r\n");
-    fputs($smtp, "\r\n.\r\n");
+    $this->command("\r\n$body");
+    $this->command("\r\n.", "250");
     
-    $result = fgets($smtp);
-    fclose($smtp);
-    return (preg_match("/^250/", $result) === 1);
+    fclose($this->smtp);
+  }
+  
+  protected function connect()
+  {
+    $this->smtp = fsockopen($this->server, $this->port);
+    
+    if (strpos(rtrim(fgets($this->smtp)), "220") !== 0) {
+      $message = "can't connect to the SMTP Server. "
+               . "HOST => '{$this->server}, PORT => '{$this->port}'";
+      
+      throw new Sabel_Mail_Smtp_Exception_ConnectionRefused($message);
+    }
+  }
+  
+  protected function plainAuth($user, $password)
+  {
+    try {
+      $command = "AUTH PLAIN " . base64_encode("{$user}\000{$user}\000{$password}");
+      $this->command($command, "235");
+    } catch (Sabel_Mail_Smtp_Exception $e) {
+      $exception = new Sabel_Mail_Smtp_Exception_AuthFailure($e->getMessage());
+      $exception->setResponseCode($e->getResponseCode());
+      throw $exception;
+    }
+  }
+  
+  protected function loginAuth($user, $password)
+  {
+    try {
+      $this->command("AUTH LOGIN", "334");
+      $this->command(base64_encode($user), "334");
+      $this->command(base64_encode($password), "235");
+    } catch (Sabel_Mail_Smtp_Exception $e) {
+      $exception = new Sabel_Mail_Smtp_Exception_AuthFailure($e->getMessage());
+      $exception->setResponseCode($e->getResponseCode());
+      throw $exception;
+    }
+  }
+  
+  protected function command($command, $expectedStatus = null)
+  {
+    fputs($this->smtp, $command . "\r\n");
+    
+    if ($expectedStatus === null) {
+      return true;
+    } else {
+      $result = rtrim(fgets($this->smtp));
+      preg_match("/^[0-9]+/", $result, $matches);
+      
+      if ($matches[0] === $expectedStatus) {
+        return true;
+      } else {
+        $message = "got unexpected response code '{$matches[0]}'.";
+        $exception = new Sabel_Mail_Smtp_Exception($message);
+        $exception->setResponseCode($matches[0]);
+        throw $exception;
+      }
+    }
   }
 }
