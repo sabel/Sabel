97eab0adb07ddcd61c8cf60e32ebd7acf19806f2
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2008-09-01 04:15:59 +0000

    minor fix
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2733 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/en/public/index.php
+++ generator/skeleton/en/public/index.php
@@ -16,10 +16,17 @@ if (!defined("ENVIRONMENT")) {
 if (strpos($_SERVER["SCRIPT_NAME"], "/index.php") >= 1) {
   define("URI_IGNORE", $ignore);
   $ignore = str_replace($_SERVER["SCRIPT_NAME"], "", $_SERVER["REQUEST_URI"]);
-  $_SERVER["REQUEST_URI"] = ltrim($ignore, DS);
-} elseif (isset($_GET["_uri"])) {
+  $_SERVER["REQUEST_URI"] = ltrim($ignore, "/");
+} elseif (isset($_GET[NO_REWRITE_PREFIX])) {
   define("NO_REWRITE", true);
-  $_SERVER["REQUEST_URI"] = $_GET["_uri"];
+  $_uri = substr(ltrim($_SERVER["REQUEST_URI"], "/"), 6);  // ?_uri=
+  $_SERVER["REQUEST_URI"] = $_uri;
+  $parsed = parse_url($_SERVER["REQUEST_URI"]);
+  if (isset($parsed["query"])) {
+    parse_str($parsed["query"], $_GET);
+  }
+  
+  unset($_GET[NO_REWRITE_PREFIX]);
 }
 
 if (ENVIRONMENT === PRODUCTION) Sabel::init();
