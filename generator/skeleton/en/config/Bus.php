c1f740836e4a6ce66e5f06930e88315bd550f189
ebine <ebine@0a2fe512-f36d-da11-803c-00304871b6ac>
2009-10-14 09:28:46 +0000

    fixed indent
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2908 0a2fe512-f36d-da11-803c-00304871b6ac

--- generator/skeleton/en/config/Bus.php
+++ generator/skeleton/en/config/Bus.php
@@ -2,28 +2,34 @@
 
 class Config_Bus extends Sabel_Bus_Config
 {
-  protected $logging    = false;
+  protected $logging = false;
   
-  protected $processors = array("addon"       => "Processor_Addon",
-                                "request"     => "Processor_Request",
-                                "response"    => "Processor_Response",
-                                "router"      => "Processor_Router",
-                                "session"     => "Processor_Session",
-                                "controller"  => "Processor_Controller",
-                                "helper"      => "Processor_Helper",
-                                "initializer" => "Processor_Initializer",
-                                "action"      => "Processor_Action",
-                                "view"        => "Processor_View");
+  protected $processors = array(
+    "addon"       => "Processor_Addon",
+    "request"     => "Processor_Request",
+    "response"    => "Processor_Response",
+    "router"      => "Processor_Router",
+    "session"     => "Processor_Session",
+    "helper"      => "Processor_Helper",
+    "controller"  => "Processor_Controller",
+    "initializer" => "Processor_Initializer",
+    "action"      => "Processor_Action",
+    "view"        => "Processor_View"
+  );
   
-  protected $interfaces = array("request"     => "Sabel_Request",
-                                "response"    => "Sabel_Response",
-                                "session"     => "Sabel_Session",
-                                "view"        => "Sabel_View",
-                                "controller"  => "Sabel_Controller_Page");
+  protected $interfaces = array(
+    "request"     => "Sabel_Request",
+    "response"    => "Sabel_Response",
+    "session"     => "Sabel_Session",
+    "view"        => "Sabel_View",
+    "controller"  => "Sabel_Controller_Page"
+  );
   
-  protected $configs    = array("map"         => "Config_Map",
-                                "addon"       => "Config_Addon",
-                                "database"    => "Config_Database");
+  protected $configs = array(
+    "map"         => "Config_Map",
+    "addon"       => "Config_Addon",
+    "database"    => "Config_Database"
+  );
   
   public function getProcessors()
   {
