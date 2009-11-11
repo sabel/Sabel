3199e85a6433ad200d965a63d9a27e8bab0c4321
morireo <morireo@0a2fe512-f36d-da11-803c-00304871b6ac>
2009-11-11 06:25:35 +0000

    remove weaver package of aspect
    git-svn-id: svn+ssh://bnsi/var/svn/sabel/trunk@2951 0a2fe512-f36d-da11-803c-00304871b6ac

--- sabel/aspect/Weaver.php
+++ sabel/aspect/Weaver.php
@@ -1,7 +1,81 @@
 <?php
 
-// @todo remove this interface
-interface Sabel_Aspect_Weaver
+class Sabel_Aspect_Weaver
 {
-  public function getProxy();
+  protected $target  = null;
+  protected $advisor = array();
+  
+  public function __construct($target = null)
+  {
+    if ($target !== null) {
+      $this->target = $target;  
+    }
+  }
+  
+  public function addAdvisor($advisor, $position = null)
+  {
+    if ($position === null) {
+      $position = count($this->advisor);
+    }
+    
+    $this->advisor[$position] = $advisor;
+  }
+  
+  /**
+   * @param object $target
+   */
+  public function setTarget($target)
+  {
+    if (class_exists($target)) {
+      $this->target = $target;  
+    } else {
+      throw new Sabel_Exception_Runtime("target must be exist class. {$target} not found");
+    }
+  }
+  
+  public function getProxy()
+  {
+    if ($this->target === null) {
+      throw new Sabel_Exception_Runtime("must be set target class");
+    }
+    
+    if (!is_object($this->target)) {
+      if (class_exists($this->target)) {
+        $this->target = new $this->target();
+      }
+    }
+    
+    $adviced    = new Sabel_Aspect_Adviced();
+    $reflection = new Sabel_Reflection_Class($this->target);
+    
+    foreach ($this->advisor as $advisor) {
+      if (!$advisor instanceof Sabel_Aspect_Advisor) {
+        throw new Sabel_Exception_Runtime("advisor must be implements Sabel_Aspect_Advisor");
+      }
+        
+      $pointcut = $advisor->getPointcut();
+      
+      if (!$pointcut instanceof Sabel_Aspect_Pointcut) {
+        throw new Sabel_Exception_Runtime("pointcut must be Sabel_Aspect_Pointcut");
+      }
+      
+      $pointcuts = new Sabel_Aspect_DefaultPointcuts();
+      
+      foreach ($reflection->getMethods() as $method) {
+        if ($pointcuts->matches($pointcut, $method->getName(), $this->target)) {
+          $adviced->addAdvices($method->getName(), $advisor->getAdvice());
+        }
+      }
+    }
+    
+    if ($adviced->hasAdvices()) {
+      $proxy = new Sabel_Aspect_Proxy_Static($this->target);
+      $proxy->__setAdviced($adviced);
+      
+      return $proxy;
+    } else {
+      // no match found. return a raw target object.
+      return $this->target;
+    }
+  }
 }
